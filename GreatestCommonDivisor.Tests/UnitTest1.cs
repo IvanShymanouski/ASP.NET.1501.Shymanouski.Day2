4d1c57408d3beefb487abb591d417ac769463f61
IvanShymanouski <Shymanouskiivan@gmail.com>
2015-03-05 23:31:33 +0200

    task3 complete, without time calculation

---
+++ GreatestCommonDivisor.Tests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GCD = GreatestCommonDivisorLibrary.GreatestCommonDivisor;
+
+namespace GreatestCommonDivisorLibrary.Tests
+{
+    [TestClass]
+    public class GreatestCommonDivisorTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource(
+           "Microsoft.VisualStudio.TestTools.DataSource.XML",
+           "|DataDirectory|\\Euclidean.xml",
+           "TestCase",
+           DataAccessMethod.Sequential)]
+        [DeploymentItem("GreatestCommonDivisor.Tests\\Euclidean.xml")]
+        [TestMethod]
+        public void EuclideanAlgorithmTest()
+        {
+            Assert.AreEqual(GCD.EuclideanAlgorithm(int.Parse((string)TestContext.DataRow["a"]), 
+                                                   int.Parse((string)TestContext.DataRow["b"])
+                                                  ),
+                            int.Parse((string)TestContext.DataRow["result"])
+                           );
+        }
+
+        [DataSource(
+         "Microsoft.VisualStudio.TestTools.DataSource.XML",
+         "|DataDirectory|\\Euclidean.xml",
+         "TestCase",
+         DataAccessMethod.Sequential)]
+        [DeploymentItem("GreatestCommonDivisor.Tests\\Euclidean.xml")]
+        [TestMethod]
+        public void SteinAlgorithmTest()
+        {
+            Assert.AreEqual(GCD.SteinAlgorithm(int.Parse((string)TestContext.DataRow["a"]),
+                                               int.Parse((string)TestContext.DataRow["b"])
+                                              ),
+                            int.Parse((string)TestContext.DataRow["result"])
+                           );
+        }
+
+        [TestMethod]
+        public void EuclideanAlgorithmTest2()
+        {
+            Assert.AreEqual(GCD.EuclideanAlgorithm(0, 500, 19), 1);
+            Assert.AreEqual(GCD.EuclideanAlgorithm(38, 500, 19), 1);
+            Assert.AreEqual(GCD.EuclideanAlgorithm(38, 950, 19, 57), 19); 
+            Assert.AreEqual(GCD.EuclideanAlgorithm(57), 57);
+        }
+
+        [TestMethod]
+        public void SteinAlgorithmTest2()
+        {
+            Assert.AreEqual(GCD.SteinAlgorithm(0, 500, 19), 1);
+            Assert.AreEqual(GCD.SteinAlgorithm(38, 500, 19), 1);
+            Assert.AreEqual(GCD.SteinAlgorithm(57), 57);
+        } 
+    }
+}
---
+++ GreatestCommonDivisor/GreatestCommonDivisor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestCommonDivisorLibrary
+{
+    public static class GreatestCommonDivisor
+    {
+        #region Euclidean
+        /// <summary>
+        /// Method calculates greatest common divisor of two numbers with help the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a"> First number </param>
+        /// <param name="b"> Second number </param>
+        /// <returns> Greatest Common divisor </returns>
+        public static int EuclideanAlgorithm(int a, int b)
+        {
+            if (a < 0) a = -a;
+            if (b < 0) b = -b;
+
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+
+        /// <summary>
+        /// Method calculates greatest common divisor of N-th numbers with help the Euclidean algorithm.
+        /// </summary>
+        /// <param name="param"> Numbers </param>
+        /// <returns> Greatest common divisor </returns>
+        public static int EuclideanAlgorithm(params int[] param)
+        {
+            switch (param.Length)
+            {
+                case 0: return 0;
+                case 1: return param[0];
+            }
+            int a = param[0];
+
+            for (int i = 1; i < param.Length; i++)
+            {
+                a = EuclideanAlgorithm(a, param[i]);
+            }
+
+            return a;
+        }
+        #endregion
+
+        #region Stein's
+        /// <summary>
+        /// Method calculates greatest common divisor of two numbers with help the Stein's algorithm.
+        /// </summary>
+        /// <param name="a"> First number </param>
+        /// <param name="b"> Second number </param>
+        /// <returns> Greatest Common divisor </returns>
+        public static int SteinAlgorithm(int a, int b)
+        {
+            int shift;
+
+            if (a < 0) a = -a;
+            if (b < 0) b = -b;
+            if (a == 0) return b;
+            if (b == 0) return a;
+            
+            //while a and b is even numbers
+            for (shift = 0; ((a | b) & 1) == 0; ++shift)
+            {
+                a >>= 1;
+                b >>= 1;
+            }
+
+            while ((a & 1) == 0)  a >>= 1;
+
+            do
+            {
+                while ((b & 1) == 0) b >>= 1;
+
+                if (a > b) Swap(ref a, ref b);  
+
+                b = b - a;                  
+            } while (b != 0);
+
+            return a << shift;
+        }
+
+        /// <summary>
+        /// Method calculates greatest common divisor of N-th numbers with help the Stein's algorithm.
+        /// </summary>
+        /// <param name="param"> Numbers </param>
+        /// <returns> Greatest common divisor </returns>
+        public static int SteinAlgorithm(params int[] param)
+        {
+            switch (param.Length)
+            {
+                case 0: return 0;
+                case 1: return param[0];
+            }
+            int a = param[0];
+
+            for (int i = 1; i < param.Length; i++)
+            {
+                a = SteinAlgorithm(a, param[i]);
+            }
+
+            return a;
+        }
+        #endregion
+
+        private static void Swap(ref int a1, ref int a2)
+        {
+            int temp = a1;
+            a1 = a2;
+            a2 = temp;
+        }
+    }
+}
